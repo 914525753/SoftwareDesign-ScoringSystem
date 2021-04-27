52e4d5f10034a1f360970d50f2f8f113991fec01
Sh4d0w_小白 <xhy914525753@163.com>
2021-04-27 15:05:03 +0800

    功能补全

--- 前端/pages/examine/accept/accept.js
+++ 前端/pages/examine/accept/accept.js
@@ -132,6 +132,11 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var type = options.type;
+    this.setData
+    ({
+      type: type
+    });
     this. GetInfo();
   },
 
